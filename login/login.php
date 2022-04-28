b4d479e5d7ef14e602047a5995281ae7496c8685
CarlaOlvera <carla.olverar7@gmail.com>
2022-04-28 03:28:06 -0500

    login 1ra parte
    aita masomenos

---
+++ login/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UFT-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <tittle>Usuario</tittle>
+
+    <link rel="stylesheet" href="">
+</head>
+<body>
+
+<div class="container">
+    <div class="content">
+        <h3>hi, <span>Usuario</span></h3>
+        <h1>Bienvenido!! <span></span></h1>
+        <p>Esta es la pagina de usuario</p>
+        <a href="registrar.php" class="btn">Registrarme</a>
+        <a href="menu.php" class="btn">Iniciar sesion</a>
+    </div>
+
+    
+</div>
+
+</body>
+</html>
